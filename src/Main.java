d8c47d6f464ee292d78e8172c22dc891b58b8e57
krassot1n <krassotin68@gmail.com>
2023-01-24 22:05:54 +0800

    сделано дз

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задача №1");
+
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
 
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача №2");
+
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача №3");
 
         dog = dog - 3.5;
         cat = cat - 1.6;
@@ -17,10 +31,14 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
 
+        System.out.println("Задача №4");
+
         var friend = 19;
         System.out.println(friend);
-        friend = (friend +2)/7;// простая математика
+        friend = (friend +2)/7;
         System.out.println(friend);
+
+        System.out.println("Задача №5");
 
         var frog = 3.5;// ((frog +10) / 3.5) + 4 так можно написать? Java прочтет так как я задумываю или что то неправит=льно может вычислится
         System.out.println(frog);
@@ -31,6 +49,9 @@ public class Main {
         frog = frog + 4;
         System.out.println(frog);
 
+        System.out.println("Задача №6");
+
+
         var boxerWeight1 = 78.2;
         var boxerWeight2 = 82.9;
         var sumWeight = boxerWeight2 + boxerWeight1;
@@ -39,6 +60,8 @@ public class Main {
         System.out.println("Разница в весе " + weigtDefference + " кг" );
         var weihtDefference1 = boxerWeight2 - boxerWeight1;
         System.out.println("вычитание из большего веса меньшего дает результат " + weihtDefference1 + " кг" );
+
+        System.out.println("Задача №8");
 
         var num = 640/8;
         System.out.println("Всего работников в компании - " + num + " человек");
